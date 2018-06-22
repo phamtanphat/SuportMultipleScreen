fba5ad3b38082dede8ae300434a84cb9ef4e5cd5
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-22 20:58:08 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/suportmultiplescreen/MainActivity.java
@@ -0,0 +1,35 @@
+package com.ptp.phamtanphat.suportmultiplescreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout relativeLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        relativeLayout = findViewById(R.id.relativelayout);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        TextView textView = new TextView(MainActivity.this);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        textView.setLayoutParams(layoutParams);
+        textView.setText("Chao ban");
+
+        relativeLayout.addView(textView);
+
+    }
+}
